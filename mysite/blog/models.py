d1d50c023b38ea5384b05a573c04745df11c1da6
KonstantinSelivanov <selivanov.km@mail.ru>
2020-11-05 23:25:06 +0500

    master

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Data model for blog posts
+# Модель данных для постов блога
+class Post(models.Model):
+    # Post status
+    # Статус постов
+    STATUS_CHOICES = ( ('draft', 'Черновик'), ('published','Опубликовано') )
+    # Post title
+    # Заголовок постов блога
+    title = models.CharField(max_length=250) 
+    # Semantic URL for posts 
+    # Семантический URL  для постов
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+    # Foreign key of the author post
+    # Внешний ключ автора поста
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
