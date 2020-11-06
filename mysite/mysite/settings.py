c8898f9be2540140b4c2244fa2bd5cde8c7995b4
KonstantinSelivanov <selivanov.km@mail.ru>
2020-11-06 12:56:48 +0500

    master

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # App configuration
+    # Конфигурация приложения
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
