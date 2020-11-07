09bce9f6d4fdd8e0ead7ad6136f4ea7ccbbd3cac
KonstantinSelivanov <selivanov.km@mail.ru>
2020-11-07 13:18:25 +0500

    master

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Post
 
 
-# Register model Post
-admin.site.register(Post)
+# Registration of a decareable class Post
+# Регистрация декарируемого класса Post
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # The fields that models will designate on the list page
+    # Поля модели которые будут отображаться на странице списка
+    list_display = ('title', 'slug', 'author', 'date_published', 'status')
+    # Filtering the post list
+    # Фильтрация списка статей
+    list_filter = ('status', 'created', 'date_published', 'author')
+    # Search
+    # Поиск
+    search_fields = ('title', 'body_preview', 'body')
+    # Automatic URL generation (slug)
+    # Автоматическая генерация URL (slug)
+    preopopulated_fields = {'slug': ('title',)}
+    # Navigation by post date
+    # Навигация по датам публикации 
+    date_hierarchy = 'date_published'
+    # Sort posts by 'status', 'date_published'
+    # Сортировка статей по 'status', 'date_published'
+    ordering = ('status', 'date_published')
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -14,31 +14,32 @@ class Post(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=250)
     # Semantic URL for posts
     # Семантический URL  для статьи
-    slug = models.SlugField('URL', max_length=250, unique_for_date='published')
+    slug = models.SlugField('URL', max_length=250, unique_for_date='date_published')
     # Foreign key of the author post
     # Внешний ключ автора статьи
     author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE, related_name='blog_posts')
+    # Preview post
+    # Предварительный просмотр статьи
+    body_preview = models.TextField(verbose_name='Предварительный просмотр статьи')
     # The content of the post
     # Содержание статьи
     body = models.TextField(verbose_name='Содержание статьи')
     # Post publication date
     # Дата публикации статьи
-    publish = models.DateTimeField('Дата публикации статьи', default=timezone.now)
+    date_published = models.DateTimeField('Дата публикации статьи', default=timezone.now)
     # Post creation date
     # Дата создания статьи
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField('Дата создания статьи', auto_now_add=True)
     # Date and time when the post was updated
     # Дата и время когда была обновлена статья
     updated = models.DateTimeField(auto_now=True)
     # Post status
     # Статус статьи
-    status = models.CharField(verbose_name='Статус',
-        max_length=10, choices=STATUS_CHOICES, default='draft')
-
+    status = models.CharField(verbose_name='Статус', max_length=10, choices=STATUS_CHOICES, default='draft')
     # Metadata. Sort order of posts in descending order of publication date
     # Метаданные. Порядок сортировки постов по убыванию даты публикации
     class Meta:
-        ordering = ('-publish',)
+        ordering = ('-date_published',)
         verbose_name = 'статью'
         verbose_name_plural = 'Статьи'
 
