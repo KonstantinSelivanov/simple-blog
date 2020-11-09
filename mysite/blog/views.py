8fa5a9a7847eb76b49fccaf9bab2c30e195b3ea6
KonstantinSelivanov <selivanov.km@mail.ru>
2020-11-10 00:19:01 +0500

    master

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -60,12 +60,10 @@ def post_detail(request, year, month, day, post):
             # Saving a comment in the database
             # Сохранение комментария в базе данных
             new_comment.save()
-        else:
-            # If the form is filled out incorrectly, the HTML template with error messages is displayed
-            # Если форма заполнена некорректно отображается HTML-шаблон с сообщениями об ошибках
-            comment_form = CommentForm()
     else:
-        pass
+        # If the form is filled out incorrectly, the HTML template with error messages is displayed
+        # Если форма заполнена некорректно отображается HTML-шаблон с сообщениями об ошибках
+        comment_form = CommentForm()
     return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
 
 # Class handler analog of the "post_list" function
